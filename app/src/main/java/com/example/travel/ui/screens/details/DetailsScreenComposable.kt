b3764e1b631e9f234375fbe14b518539d7d5d239
askat <uaskarbek92@gmail.com>
2025-02-02 23:39:22 +0600

    Implement Image block in Details screen

--- app/src/main/java/com/example/travel/ui/screens/details/DetailsScreenComposable.kt
+++ app/src/main/java/com/example/travel/ui/screens/details/DetailsScreenComposable.kt
@@ -3,15 +3,34 @@ package com.example.travel.ui.screens.details
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.travel.MainViewModel
+import com.example.travel.R
 import com.example.travel.ui.screens.details.composables.ImageCard
 
 @OptIn(ExperimentalSharedTransitionApi::class)
@@ -24,10 +43,44 @@ fun DetailsScreenComposable(
     viewModel: MainViewModel = viewModel()
 ) {
     val place by viewModel.getPlace(placeId).collectAsStateWithLifecycle(null)
+    val scrollSate = rememberScrollState()
 
     place?.let { p ->
-        Scaffold { paddingValues ->
-            Column(modifier = Modifier.padding(paddingValues)) {
+        Scaffold(
+            floatingActionButton = {
+                Button(
+                    onClick = {
+
+                    },
+                    shape = RoundedCornerShape(20.dp),
+                    elevation = ButtonDefaults.elevatedButtonElevation(
+                        defaultElevation = 4.dp,
+                        pressedElevation = 8.dp
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    Text(
+                        stringResource(R.string.book_now),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Icon(
+                        painter = painterResource(R.drawable.send_icon),
+                        contentDescription = stringResource(R.string.book_now),
+                    )
+                }
+            },
+            floatingActionButtonPosition = FabPosition.Center
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .scrollable(state = scrollSate, orientation = Orientation.Vertical)
+            ) {
                 ImageCard(
                     place = p,
                     sharedTransitionScope,
